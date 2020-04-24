d609ba52f09380812ff96e9c6ba791a66a9d673b
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2020-04-24 10:59:32 +0200

    EN DEV -- ajout de parcel et import

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 // kana === hiragana or katagana
 // romanji === latin alphabet
+
+import {
+  completeKana,
+  nextRandom,
+  splitAKana
+} from './js/helpers.js'
+
 const arrayKana = []
 
 let iNextRandom
@@ -116,29 +123,6 @@ function changeExercise () {
   kanaRomanji.value = ''
 }
 
-function completeKana (kana) {
-  let urlKana
+export {
 
-  if (kana[0] === 'h') {
-    urlKana = 'img/hiragana/' + kana[1] + '.png'
-  } else if (kana[0] === 'k') {
-    urlKana = 'img/katakana/' + kana[1] + '.png'
-  }
-
-  return urlKana
-}
-
-function nextRandom () {
-  let iRandom = getRandomInt(arrayKana.length)
-  return iRandom
-}
-
-function getRandomInt (max) {
-  return Math.floor(Math.random() * Math.floor(max))
-}
-
-function splitAKana (sKana) {
-  let aKana
-  aKana = sKana.split('-')
-  return aKana
 }
---
+++ js/helpers.js
@@ -0,0 +1,36 @@
+import {
+
+} from '../index.js'
+
+function completeKana (kana) {
+  let urlKana
+
+  if (kana[0] === 'h') {
+    urlKana = 'img/hiragana/' + kana[1] + '.png'
+  } else if (kana[0] === 'k') {
+    urlKana = 'img/katakana/' + kana[1] + '.png'
+  }
+
+  return urlKana
+}
+
+function nextRandom () {
+  let iRandom = getRandomInt(arrayKana.length)
+  return iRandom
+}
+
+function getRandomInt (max) {
+  return Math.floor(Math.random() * Math.floor(max))
+}
+
+function splitAKana (sKana) {
+  let aKana
+  aKana = sKana.split('-')
+  return aKana
+}
+
+export {
+  completeKana, 
+  nextRandom,
+  splitAKana
+}

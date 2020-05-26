eabe4713ec2d2a38abaf4285692e86c52ed4865b
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2020-05-26 22:37:33 +0200

    bind ok et bug timout ko

--- component/guess.js
+++ component/guess.js
@@ -56,28 +56,11 @@ class Guess {
   }
 
   makeAChoice (choiceSelected, oneLesson) {
-    const myMethod = function () {
-      this.nextKana(oneLesson)
-    }.bind(this)
-
-    const countdown = getStatusLessonInStorage('countdown')
-
     oneLesson.played++
 
     this.setResultTrueOrFalse(choiceSelected, oneLesson)
 
-    if (countdown === '0') {
-      if (oneLesson.toplay === oneLesson.played) {
-        // time is finished and forcast to play is finished : lesson is done
-        oneLesson.stop()
-      } else {
-        // time finished and forecast to play : finish after current guess, do not launch again nextKana
-        // wait for choice for the current kana to guess
-      }
-    } else {
-      // time is not finished, launch again nextKana
-      setTimeout(myMethod, 400)
-    }
+    this.stopOrContinue(choiceSelected, oneLesson)
   }
 
   setResultTrueOrFalse (choiceSelected, oneLesson) {
@@ -88,6 +71,29 @@ class Guess {
       displayColorChoice(this.choiceSelectedIndex + 1, colorTrueButton)
     } else {
       displayColorChoice(this.choiceSelectedIndex + 1, colorFalseButton)
+    }
+  }
+
+  stopOrContinue (choiceSelected, oneLesson) {
+    const myMethod = () => this.nextKana(oneLesson)
+    const countdown = getStatusLessonInStorage('countdown')
+
+    if (countdown === '0') {
+      this.stop(oneLesson)
+    } else {
+      // time is not finished, launch again nextKana
+      // because of timeout, nextKana can be launch but it should not be, will be stoppped in nextKana
+      setTimeout(myMethod, 400)
+    }
+  }
+
+  stop (oneLesson) {
+    if (oneLesson.toplay === oneLesson.played) {
+      // time is finished and forcast to play is finished : lesson is done
+      oneLesson.stop()
+    } else {
+      // time finished and forecast to play : finish after current guess, do not launch again nextKana
+      // wait for choice for the current kana to guess
     }
   }
 
@@ -115,34 +121,39 @@ class Guess {
   nextKana (oneLesson) {
     let nextRandomIndex = nextRandom(oneLesson.kanaToStudy.length)
     let arrayToWrite = []
+    const countdown = getStatusLessonInStorage('countdown')
 
-    // todo : faire un new !
+    if (countdown === '0') {
+      this.stop(oneLesson)
+    } else {
+      // todo : faire un new !
 
-    this.guessWhat = nextRandom(2)
-    displayWhatToGuess(this.guessWhat)
+      this.guessWhat = nextRandom(2)
+      displayWhatToGuess(this.guessWhat)
 
-    arrayToWrite = randomizeChoice(this.guessWhat, oneLesson.kanaToStudy)
-    writeChoice(this.guessWhat, oneLesson.nbChoice, arrayToWrite)
+      arrayToWrite = randomizeChoice(this.guessWhat, oneLesson.kanaToStudy)
+      writeChoice(this.guessWhat, oneLesson.nbChoice, arrayToWrite)
 
-    this.previousKana = this.kana
-    this.kana = oneLesson.kanaToStudy[nextRandomIndex]
-
-    while (this.previousKana.letter === this.kana.letter) {
-      nextRandomIndex = nextRandom(oneLesson.kanaToStudy.length)
+      this.previousKana = this.kana
       this.kana = oneLesson.kanaToStudy[nextRandomIndex]
-    }
 
-    this.writeChoiceTrueFalse(arrayToWrite)
-    clearChoice(oneLesson.nbChoice)
+      while (this.previousKana.letter === this.kana.letter) {
+        nextRandomIndex = nextRandom(oneLesson.kanaToStudy.length)
+        this.kana = oneLesson.kanaToStudy[nextRandomIndex]
+      }
 
-    // write choice if number of kana to guess > of nb of choice
-    // need  oneGuess.init to display the true choice
-    if (oneLesson.kanaToStudy.length > 5) {
-      arrayToWrite = writeChoiceMoreThanNbChoicePossible(this.guessWhat, oneLesson.nbChoice, arrayToWrite, this.choiceTrueIndex)
       this.writeChoiceTrueFalse(arrayToWrite)
-    }
+      clearChoice(oneLesson.nbChoice)
 
-    this.guessKana(oneLesson)
+      // write choice if number of kana to guess > of nb of choice
+      // need  oneGuess.init to display the true choice
+      if (oneLesson.kanaToStudy.length > 5) {
+        arrayToWrite = writeChoiceMoreThanNbChoicePossible(this.guessWhat, oneLesson.nbChoice, arrayToWrite, this.choiceTrueIndex)
+        this.writeChoiceTrueFalse(arrayToWrite)
+      }
+
+      this.guessKana(oneLesson)
+    }
   }
 }
 

38aa70cb4d1d6713492e9af621369ad23acb5b99
Sylvie PONTHUS-VIOLLAND <sylvie.ponthus@amac02c8694md6r.home>
2021-02-15 20:33:36 +0100

    add test lessonCatalog

--- component/LessonCatalog.js
+++ component/LessonCatalog.js
@@ -6,36 +6,120 @@ export class LessonCatalog {
                 {
                     id: "a",
                     title: "a i u e o",
+                    group: "basic",
                 },
                 {
                     id: "ka",
                     title: "ka ki ku ke ko",
+                    group: "basic",
                 },
                 {
                     id: "sa",
                     title: "sa shi su se so",
+                    group: "basic",
                 },
                 {
                     id: "ta",
                     title: "ta chi tsu te to",
+                    group: "basic",
+                },
+                {
+                    id: "ga",
+                    title: "ga gi gu ge go",
+                    group: "dakuon",
+                },
+                {
+                    id: "za",
+                    title: "za ji zu ze zo",
+                    group: "dakuon",
+                },
+                {
+                    id: "da",
+                    title: "da ji(di) zu(du) de do",
+                    group: "dakuon",
+                },
+                {
+                    id: "ba",
+                    title: "ba bi bu be bo",
+                    group: "dakuon",
+                },
+                {
+                    id: "pa",
+                    title: "pa pi pu pe po",
+                    group: "handakuon",
                 },
             ],
             "katakana": [
                 {
                     id: "a",
                     title: "a i u e o",
+                    group: "basic",
                 },
                 {
                     id: "ka",
                     title: "ka ki ku ke ko",
+                    group: "basic",
+                },
+                {
+                    id: "ga",
+                    title: "ga gi gu ge go",
+                    group: "dakuon",
+                },
+                {
+                    id: "za",
+                    title: "za ji zu ze zo",
+                    group: "dakuon",
+                },
+                {
+                    id: "da",
+                    title: "da ji(di) zu(du) de do",
+                    group: "dakuon",
+                },
+                {
+                    id: "ba",
+                    title: "ba bi bu be bo",
+                    group: "dakuon",
+                },
+                {
+                    id: "pa",
+                    title: "pa pi pu pe po",
+                    group: "handakuon",
                 },
             ]
         }
     }
 
-    getId(id) {
+    getHiraganaId(id) {
         let lessons = this.list()
         lessons = lessons.hiragana.filter(lesson => lesson.id === id)
+        return lessons
+    }
+
+    getKatakanaId(id) {
+        let lessons = this.list()
+        lessons = lessons.katakana.filter(lesson => lesson.id === id)
+        return lessons
+    }
+
+    getHiraganaList() {
+        const lessons = this.list()
+        return lessons.hiragana
+    }
+
+    getKatakanaList() {
+        const lessons = this.list()
+        return lessons.katakana
+    }
+
+    getHiraganaByGroup(group) {
+        let lessons = this.list()
+        lessons = lessons.hiragana.filter(lesson => lesson.group === group)
+        return lessons
+    }
+
+    getKatakanaByGroup(group) {
+        let lessons = this.list()
+        lessons = lessons.katakana.filter(lesson => lesson.group === group)
         return lessons
     }
 
--- test/LessonCatalog.test.js
+++ test/LessonCatalog.test.js
@@ -2,57 +2,175 @@ import {LessonCatalog} from "../component/LessonCatalog";
 
 import assert from "assert";
 
+const hiraganaBasicList = [
+    {
+        id: "a",
+        title: "a i u e o",
+        group: "basic",
+    },
+    {
+        id: "ka",
+        title: "ka ki ku ke ko",
+        group: "basic",
+    },
+    {
+        id: "sa",
+        title: "sa shi su se so",
+        group: "basic",
+    },
+    {
+        id: "ta",
+        title: "ta chi tsu te to",
+        group: "basic",
+    },
+]
+
+const hiraganaDakuonList = [
+    {
+        id: "ga",
+        title: "ga gi gu ge go",
+        group: "dakuon",
+    },
+    {
+        id: "za",
+        title: "za ji zu ze zo",
+        group: "dakuon",
+    },
+    {
+        id: "da",
+        title: "da ji(di) zu(du) de do",
+        group: "dakuon",
+    },
+    {
+        id: "ba",
+        title: "ba bi bu be bo",
+        group: "dakuon",
+    },
+]
+
+const hiraganaHandakuonList = [
+    {
+        id: "pa",
+        title: "pa pi pu pe po",
+        group: "handakuon",
+    },
+]
+
+const hiraganaList = hiraganaBasicList.concat(hiraganaDakuonList).concat(hiraganaHandakuonList)
+
+const katakanaBasicList = [
+    {
+        id: "a",
+        title: "a i u e o",
+        group: "basic",
+    },
+    {
+        id: "ka",
+        title: "ka ki ku ke ko",
+        group: "basic",
+    },
+]
+
+const katakanaDakuonList = [
+    {
+        id: "ga",
+        title: "ga gi gu ge go",
+        group: "dakuon",
+    },
+    {
+        id: "za",
+        title: "za ji zu ze zo",
+        group: "dakuon",
+    },
+    {
+        id: "da",
+        title: "da ji(di) zu(du) de do",
+        group: "dakuon",
+    },
+    {
+        id: "ba",
+        title: "ba bi bu be bo",
+        group: "dakuon",
+    },
+]
+
+const katakanaHandakuonList = [
+    {
+        id: "pa",
+        title: "pa pi pu pe po",
+        group: "handakuon",
+    },
+]
+
+const katakanaList = katakanaBasicList.concat(katakanaDakuonList).concat(katakanaHandakuonList)
+
 
 describe('LessonCatalog', function () {
     it('should provide the hiragana list', function () {
-        const actual = [
-            {
-                id: "a",
-                title: "a i u e o",
-            },
-            {
-                id: "ka",
-                title: "ka ki ku ke ko",
-            },
-            {
-                id: "sa",
-                title: "sa shi su se so",
-            },
-            {
-                id: "ta",
-                title: "ta chi tsu te to",
-            },
-        ]
+        const actual = hiraganaList
         const sut = new LessonCatalog()
-        const result = sut.list()
-        assert.deepEqual(actual, result.hiragana)
+        const result = sut.getHiraganaList()
+        assert.deepEqual(actual, result)
     })
 
     it('should provide the katakana list', function () {
-        const actual = [
-            {
-                id: "a",
-                title: "a i u e o",
-            },
-            {
-                id: "ka",
-                title: "ka ki ku ke ko",
-            },
-        ]
+        const actual = katakanaList
         const sut = new LessonCatalog()
-        const result = sut.list()
-        assert.deepEqual(actual, result.katakana)
+        const result = sut.getKatakanaList()
+        assert.deepEqual(actual, result)
     })
 
-    it('should provide the "a" lesson', function () {
+    it('should provide the basic hiragana list', function () {
+        const actual = hiraganaBasicList
+        const sut = new LessonCatalog()
+        const result = sut.getHiraganaByGroup("basic")
+        assert.deepEqual(actual, result)
+    })
+
+    it('should provide the basic katakana list', function () {
+        const actual = katakanaBasicList
+        const sut = new LessonCatalog()
+        const result = sut.getKatakanaByGroup("basic")
+        assert.deepEqual(actual, result)
+    })
+
+    it('should provide the dakuon hiragana list', function () {
+        const actual = hiraganaDakuonList
+        const sut = new LessonCatalog()
+        const result = sut.getHiraganaByGroup("dakuon")
+        assert.deepEqual(actual, result)
+    })
+
+    it('should provide the handakuon hiragana list', function () {
+        const actual = hiraganaHandakuonList
+        const sut = new LessonCatalog()
+        const result = sut.getHiraganaByGroup("handakuon")
+        assert.deepEqual(actual, result)
+    })
+
+    it('should provide the hiragana "a" lesson', function () {
         const actual = [
             {
                 id: "a",
                 title: "a i u e o",
+                group: "basic",
             },
         ]
         const sut = new LessonCatalog()
-        const result = sut.getId("a")
+        const result = sut.getHiraganaId("a")
+        assert.deepEqual(actual, result)
+    })
+
+    it('should provide the katakana "a" lesson', function () {
+        const actual = [
+            {
+                id: "a",
+                title: "a i u e o",
+                group: "basic",
+            },
+        ]
+        const sut = new LessonCatalog()
+        const result = sut.getKatakanaId("a")
         assert.deepEqual(actual, result)
     })
 

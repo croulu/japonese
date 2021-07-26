3c9d6a3969e0255e6d6542d30d230f4bf2bcc3df
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2021-07-26 09:51:07 +0200

    refacto ko

--- component/Learn/PlayItem/PlayKana.js
+++ component/Learn/PlayItem/PlayKana.js
@@ -4,28 +4,15 @@ import {Choice} from "./Choice";
 
 export const PlayKana = ({letters, alphabet, guessWhat, guessKana}) => {
 
-    if (guessWhat === "kana") {
-
-        return <div id="playKana">
-            <div className="playKana">
-                <div className="w3-hover-none">
-                    <div className="w3-red w3-text-black playKanaChoice w3-hover-none" id="playItemRomanji">{guessKana}</div>
-                </div>
-                {letters.map(letter => <Choice key={letter.txt} letter={letter} alphabet={alphabet}
-                                               guessWhat={guessWhat}/>)}
+    return <div id="playKana">
+        <div className="playKana">
+            <div className="w3-hover-none">
+                <div className="w3-red w3-text-black playKanaChoice w3-hover-none"
+                     id="playItemRomanji">{guessWhat}-{guessKana}</div>
+                <div className="w3-red playKanaChoice w3-hover-none" id="playItemKana"><span id="kanaImg"></span></div>
             </div>
-        </div>;
-    } else {
-        return <div id="playKana">
-            <div className="playKana">
-                <div className="w3-hover-none">{guessKana}
-                    <div className="w3-red playKanaChoice w3-hover-none" id="playItemKana"><span id="kanaImg"></span>
-                    </div>
-                </div>
-                {letters.map(letter => <Choice key={letter.txt} letter={letter} alphabet={alphabet}
-                                               guessWhat={guessWhat}/>)}
-            </div>
-        </div>;
-
-    }
+            {letters.map(letter => <Choice key={letter.txt} letter={letter} alphabet={alphabet}
+                                           guessWhat={guessWhat}/>)}
+        </div>
+    </div>
 }

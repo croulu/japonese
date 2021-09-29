5497734d253330d039ec568a2ace45f73addcb3d
Sylvie PONTHUSVIOLLAND <sponthus@gmail.com>
2021-09-29 12:03:45 +0200

    barre hero

--- component/Home/HomeHero.js
+++ component/Home/HomeHero.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const HomeHero = ({}) =>
     <>
-        <div className="page-block-content bb mt mb">
+        <div className="page-block-content mt mb">
             <h1 className=" hero-main hero-main-title">Apprenez vos kanas !</h1>
             <p className=" hero-main hero-main-subtitle">Bon, d'accord ce n'est pas facile, nous allons vous aider ;)</p>
         </div>

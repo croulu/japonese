c0541ba3581054b59b0b2f804d0c2671ddd16031
Floris Tisseyre <floris.tisseyre@sundayapp.com>
2021-10-30 21:51:22 +0200

    refacto: remove commented code

--- index.js
+++ index.js
@@ -7,19 +7,9 @@ import '@fortawesome/fontawesome-free/js/all.js'
 
 import {LessonCatalog} from "./domain/LessonCatalog";
 
-import {initClickOnCountdown} from "./js/timerCountDown";
-
 const lessonCatalog = new LessonCatalog();
 
-
-
 ReactDOM.render(
-    <App alphabets={lessonCatalog.list()} />,
-    document.getElementById("app")
+  <App alphabets={lessonCatalog.list()}/>,
+  document.getElementById("app")
 )
-
-
-
-//initClickOnCountdown();
-
-
